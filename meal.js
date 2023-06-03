34f58f831e1be546972947922e865af6e7cd3f2f
Saidul-06 <saidul15-2703@diu.edu.bd>
2023-06-03 21:41:28 +0600

    meal_middle

--- meal.js
+++ meal.js
@@ -9,7 +9,7 @@ const displayMeals = (meals) => {
   const ParentDiv = document.getElementById("Container");
   ParentDiv.innerHTML = "";
   meals.forEach((meal) => {
-    console.log(meal);
+    console.log(meal.idMeal);
     const mealdiv = document.createElement("div");
     mealdiv.classList.add("col");
     mealdiv.innerHTML = `
@@ -18,7 +18,7 @@ const displayMeals = (meals) => {
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#MealDetails">
+                <button type="button" onclick="loadMealDetails(${meal.idMeal})" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#MealDetails">
   Details
 </button>
             </div>
@@ -36,4 +36,16 @@ SearchMeal = () => {
   loadMEals(searchText);
 };
 
+const loadMealDetails = idMeal =>{
+  const url = (`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
+  fetch(url)
+    .then((Res) => Res.json())
+    .then((data) => displaymealdetails(data.meals[0]));
+}
+
+const displaymealdetails = meal => {
+  document.getElementById('MealDetailsLabel').innerText = meal.strMeal;
+  document.getElementById('mealDetailsImg').innerHTML = 
+  `<img class= "img-fluid" src="${meal.strMealThumb}" alt="">`
+}
 loadMEals("rice");
